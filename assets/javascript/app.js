2436e8bbe89969cd3e31fe9883f32d292d7e96a2
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-06-24 12:55:54 -0600

    deleted extra index, Init app.js, and linked files

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAWhGiAZ7wZVq3md9S54IfLqDVVITJpKB4",
+    authDomain: "firestore-practice1.firebaseapp.com",
+    databaseURL: "https://firestore-practice1.firebaseio.com",
+    projectId: "firestore-practice1",
+    storageBucket: "firestore-practice1.appspot.com",
+    messagingSenderId: "149291352027",
+    appId: "1:149291352027:web:5730c448c25de773"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database()
