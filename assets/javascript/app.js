265b5c13688692a237d70ed7341cc21283bf618b
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-06-26 19:29:17 -0600

    added require and lite cheerio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,46 +9,58 @@ var firebaseConfig = {
     appId: "1:149291352027:web:5730c448c25de773"
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-var database = firebase.firestore();
+// var database = firebase.firestore();
 
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": https://gitlab.example.de/api/v4/projects/"+id+"/repository/commits,
-    "method": "GET",
-    "headers": {
-      "PRIVATE-TOKEN": "TOKEN"
-    }
-  }
-  
-var response;
-$("#goRepoLink").click(function () {
-    var repoLink = $("#repoLink").val()
-    queryUrl = "https://gitlab.example.com/api/v4/projects/" + repoLink + "/repository/tree"
-})
+var saltLakeEvents = function(){
+const cheerio = require('cheerio');
+const $ = cheerio.load('https://www.visitsaltlake.com/events/free-events/');
 
-//ajax
-var settings = {
-    type: "GET",
-    "async": true,
-    "crossDomain": true,
-    url: "https://gitlab.example.de/api/v4/projects/"+id+"/repository/commits",
-    data: "data",
-    "method": "GET",
-    "headers": {
-      "PRIVATE-TOKEN": "TOKEN"
-    },
-    error: function (response) {
-        $("#errorText").text(JSON.stringify(response))
-        $('#errorModal').modal('toggle')
-    },
-  }
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+return $("div.results").text()
+
+}
+
+console.log(saltLakeEvents())
+ //-------Extra ajax code--------//
+
+// var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": https://gitlab.example.de/api/v4/projects/"+id+"/repository/commits,
+//     "method": "GET",
+//     "headers": {
+//         "PRIVATE-TOKEN": "TOKEN"
+//     }
+// }
+
+// var response;
+// $("#goRepoLink").click(function () {
+//     var repoLink = $("#repoLink").val()
+//     var siteUrl = $("#repoLink").val()
+//     queryUrl = "https://gitlab.example.com/api/v4/projects/" + repoLink + "/repository/tree"
+// })
+
+// //ajax
+// var settings = {
+//     type: "GET",
+//     "async": true,
+//     "crossDomain": true,
+//     url: https://hxtsoafqna.execute-api.us-east-1.amazonaws.com/stage/api,
+//     data: "data",
+//     "method": "GET",
+//     "headers": {
+//         "PRIVATE-TOKEN": "TOKEN"
+//     },
+//     error: function (response) {
+//         $("#errorText").text(JSON.stringify(response))
+//         $('#errorModal').modal('toggle')
+//     },
+// }
+
+// $.ajax(settings).done(function (response) {
+//     console.log(response);
+// });
 
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
