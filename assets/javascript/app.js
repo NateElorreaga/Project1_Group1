e4462343e6b58d40cbf9d233d52086505ccd0b13
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-06-29 09:15:49 -0600

    working to append filters to url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,47 +16,59 @@ var firebaseConfig = {
 
 // var database = firebase.firestore();
 
- //-------Extra ajax code--------//
+//-------Extra ajax code--------//
 
 var response;
-var userInput;
-$("#searchButton").click(function(){
-    userInput= {
-    keyWord: '&keyword='+$("#search").val(),
-    zipCode: "&postalCode="+$("#zipCode").val(),
-    city: "&city="+$("#city").val(),
-    startDate: "&startDateTime="+$("#startDate").val(),
-    endDate: "&endDateTime="+$("#endDate").val(),
-    sort: "&sort="+$("#sort").val(),
+//$("#searchButton").click(function () {
 
-    //Sorting order of the search result. Allowed values : 'name,asc', 'name,desc', 'date,asc', 'date,desc', 
-    //'relevance,asc', 'relevance,desc', 'distance,asc', 'name,date,asc', 'name,date,desc', 
-    //'date,name,asc', 'date,name,desc', 'distance,date,asc', 'onSaleStartDate,asc', 'id,asc', 
-    //'venueName,asc', 'venueName,desc', 'random'
-    genre: "&classificationName="+$("#genre").val(),
-}
+    //-----------------------------------------------------------------------//
 
-//initializes ajax settings
-var settings = {
-    url: "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=wgvkeg8fAF8kBUpnimtGl3TRrktNnitx",
-    "method": "GET",
-    error: function (response) {
-        $("#errorText").text(JSON.stringify(response))
-        $('#errorModal').modal('toggle')
+    //initializes ajax settings
+    var settings = {
+        url: "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=wgvkeg8fAF8kBUpnimtGl3TRrktNnitx",
+        "method": "GET",
+        "error": function (response) {
+            $("#errorText").text(JSON.stringify(response))
+            $('#errorModal').modal('toggle')
+        }
     }
-}
 
-// for looping over userInputs to make sure empty properties dont break the link with undefined
-for(prop in userInput){
-    if (userInput.prop===undefined || userInput.prop===null){userInput.prop = ""}
-    else {settings.url = settings.url + userInput.prop}
-}
+    var userInput = {
+        keyWord: "&keyword=" + $("#search").val(),
+        zipCode: "&postalCode=" + $("#zipCode").val(),
+        city: "&city=" + $("#city").val(),
+        startDate: "&startDateTime=" + $("#startDate").val(),
+        endDate: "&endDateTime=" + $("#endDate").val(),
+        genre: "&classificationName=" + $("#genre").val(),
+        sort: "&sort=" + $("#sort").val(),
+        //Sorting order of the search result. Allowed values : 'name,asc', 'name,desc', 'date,asc', 'date,desc', 
+        //'relevance,asc', 'relevance,desc', 'distance,asc', 'name,date,asc', 'name,date,desc', 
+        //'date,name,asc', 'date,name,desc', 'distance,date,asc', 'onSaleStartDate,asc', 'id,asc', 
+        //'venueName,asc', 'venueName,desc', 'random'
+    }
 
-// Calls ajax using link put together above
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
-})
+    // for looping over userInputs to make sure empty properties dont break the link with undefined
+    for (prop in userInput) {
+        console.log(userInput.prop)
+        if (userInput.prop===undefined){userInput.prop = "" }
+        settings.url = settings.url + userInput.prop
+    }
+
+    // sets default location to salt lake city
+    // userInput.city = 
+
+
+    // double checks url before sent to api for response
+    console.log(settings.url)
+
+    console.log(userInput)
+    //---------------------------------------------------------------------------------------//
+
+    // Calls ajax using link put together above
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+//})
 
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
